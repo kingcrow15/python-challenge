d761021d48a42b09d86cc3f3b6d34efee8d192dc
kingcrow15 <61525901+kingcrow15@users.noreply.github.com>
2020-03-21 23:50:33 -0700

    Added homework

---
+++ elections.py
@@ -0,0 +1,33 @@
+import os
+import csv
+from datetime import datetime
+from dateutil import relativedelta
+from collections import defaultdict
+
+election_file_path = os.path.join(os.path.expanduser("~"), "Documents", "GitHub", "python-challenge", "election_data_copy.csv")
+
+
+def get_election_stats(file_path):
+    with open(file_path) as csvfile:
+        rows = csv.reader(csvfile)
+        count = 0
+        candidates = defaultdict(int)
+
+        for voter_id, county, candidate in rows:
+            if voter_id == "Voter ID":
+                continue
+            count += 1
+            candidates[candidate] += 1
+
+        print("Total votes cast: {}".format(count))
+        print("List of candidates: {}".format(candidates.keys()))
+        print("Votes for each candidate: {}".format(candidates.items()))
+        percents = {candidate: str(100 * votes / count) for candidate, votes in candidates.items()}
+        print("Percetage votes for each candidate: {}".format(percents.items()))
+        most_votes = max([votes for votes in candidates.values()])
+        for candidate, votes in candidates.items():
+            if votes == most_votes:
+                print("Winner: {}".format(candidate))
+
+
+get_election_stats(election_file_path)
---
+++ python-challenge.py
@@ -0,0 +1,60 @@
+import os
+import csv
+from datetime import datetime
+from dateutil import relativedelta
+from collections import defaultdict
+
+budget_file_path = os.path.join(os.path.expanduser("~"), "Desktop", "budget_data_copy.csv")
+
+
+def get_date(date):
+    try:
+        return datetime.strptime(date, '%b-%y')  # https://stackabuse.com/converting-strings-to-datetime-in-python/
+    except ValueError:
+        pass
+
+
+def get_budget_stats(file_path):
+    with open(file_path) as csvfile:
+        rows = csv.reader(csvfile)
+        total = 0
+        first_date = None
+        last_date = None
+        count = 0
+        largest_increase = 0
+        increase_date = None
+        largest_decrease = 0
+        decrease_date = None
+        current_value = None
+        last_value = None
+
+        for row in rows:
+            if not first_date:
+                first_date = get_date(row[0])
+            last_date = get_date(row[0])
+            try:
+                last_value = current_value
+                current_value = int(row[1])
+                if last_value:
+                    difference = current_value - last_value
+                    if difference > largest_increase:
+                        largest_increase = difference
+                        increase_date = row[0]
+                    if difference < largest_decrease:
+                        largest_decrease = difference
+                        decrease_date = row[0]
+                total += int(current_value)
+                count += 1
+            except ValueError:
+                pass
+
+        print("profit/loss total is ... " + str(total))
+        print("First date: {}".format(first_date))
+        print("Last date: {}".format(last_date))
+        print(last_date - first_date)
+        difference = relativedelta.relativedelta(last_date, first_date)
+        months = 12 * difference.years + difference.months
+        print("Number of months: {}".format(months))
+        print("Average profit/loss: {}".format(total / count))
+        print("Largest increase was {} on {}".format(largest_increase, increase_date))
+        print("Largest decrease was {} on {}".format(largest_decrease, decrease_date))
